b33a228ff17bfea5299b28a720d912c214885f32
Islam <asoeslam@gmail.com>
2024-09-28 15:16:46 +0300

    Create main.tf

---
+++ modules/ec2/main.tf
@@ -0,0 +1,40 @@
+resource "aws_security_group" "allow_ssh" {
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
+resource "aws_instance" "this" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+  security_groups = [aws_security_group.allow_ssh.name]
+
+  tags = {
+    Name = var.name
+  }
+}
+
+output "instance_id" {
+  value = aws_instance.this.id
+}
+
+output "public_ip" {
+  value = aws_instance.this.public_ip
+}
