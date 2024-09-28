a0e0514b33cd27107721ec2dae40e6436a8c72b6
Islam <asoeslam@gmail.com>
2024-09-28 15:19:02 +0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,44 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "s3_bucket" {
+  description = "S3 bucket for storing Terraform state"
+  type        = string
+}
+
+variable "dynamodb_table" {
+  description = "DynamoDB table for state locking"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR block for the private subnet"
+  type        = string
+  default     = "10.0.2.0/24"
+}
+
+variable "ami_id" {
+  description = "AMI ID for the EC2 instances"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Instance type for the EC2 instances"
+  type        = string
+  default     = "t2.micro"
+}
